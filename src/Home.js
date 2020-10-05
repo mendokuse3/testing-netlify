935abff9cb2f88f178c3fcde1c6c35484a59cb13
Leo <hamlap6@gmail.com>
2020-10-05 13:54:41 -0700

    fixed warnings and styling of dashboard'

--- src/Home.js
+++ src/Home.js
@@ -5,25 +5,25 @@ function Home(props) {
     return (
         <div className='homeContainer' onClick={() => props.goToDash()}>
             <div className='home'>
-                <img className='topImg' src='https://user-images.githubusercontent.com/65630204/95115732-f3bdb000-0713-11eb-8a99-b13d38e98ef1.png' />
+                <img className='topImg' src='https://user-images.githubusercontent.com/65630204/95115732-f3bdb000-0713-11eb-8a99-b13d38e98ef1.png' alt='homepage imgs' />
 
-                <img className='trumpHomeCandi' src='https://user-images.githubusercontent.com/65630204/94746267-5eed3800-034a-11eb-8806-08c7f0a30983.png' />
+                <img className='trumpHomeCandi' src='https://user-images.githubusercontent.com/65630204/94746267-5eed3800-034a-11eb-8806-08c7f0a30983.png' alt='homepage imgs' />
 
-                <img className='bidenHomeCandi' src='https://user-images.githubusercontent.com/65630204/94747127-36663d80-034c-11eb-841d-e49a44b9940d.png' />
+                <img className='bidenHomeCandi' src='https://user-images.githubusercontent.com/65630204/94747127-36663d80-034c-11eb-841d-e49a44b9940d.png' alt='homepage imgs' />
 
-                <img className='harrisHomeCandi' src='https://user-images.githubusercontent.com/65630204/94747453-f05da980-034c-11eb-98d5-d0e08257404a.png' />
+                <img className='harrisHomeCandi' src='https://user-images.githubusercontent.com/65630204/94747453-f05da980-034c-11eb-98d5-d0e08257404a.png' alt='homepage imgs' />
 
-                <img className='penceHomeCandi' src='https://user-images.githubusercontent.com/65630204/94747954-24859a00-034e-11eb-9e6e-6167a87e6ad6.png' />
+                <img className='penceHomeCandi' src='https://user-images.githubusercontent.com/65630204/94747954-24859a00-034e-11eb-9e6e-6167a87e6ad6.png' alt='homepage imgs' />
 
                 <div className='titleContainer'>
-                    <img className='homeTitle' src='https://user-images.githubusercontent.com/65630204/95113739-e7842380-0710-11eb-80e9-cd463fc05f8d.png' />
+                    <img className='homeTitle' src='https://user-images.githubusercontent.com/65630204/95113739-e7842380-0710-11eb-80e9-cd463fc05f8d.png' alt='homepage imgs' />
                     <br />
-                    <img className='homeSubtitle' src='https://user-images.githubusercontent.com/65630204/95113744-e94de700-0710-11eb-9830-c415a71a13aa.png' />
+                    <img className='homeSubtitle' src='https://user-images.githubusercontent.com/65630204/95113744-e94de700-0710-11eb-9830-c415a71a13aa.png' alt='homepage imgs' />
 
                 </div>
 
 
-                <img className='bottomImg' src='https://user-images.githubusercontent.com/65630204/95116263-c1f91900-0714-11eb-9f91-517cd3f2bc77.png' />
+                <img className='bottomImg' src='https://user-images.githubusercontent.com/65630204/95116263-c1f91900-0714-11eb-9f91-517cd3f2bc77.png' alt='homepage imgs' />
             </div>
 
         </div>
